21805078fb0c5ce4a47be7f1ca71812b0d0823a1
Marko <maaarko@outlook.com>
2017-07-30 18:02:14 +0200

    Work on HEAT!

---
+++ Engineer.Project/GameHelpers.cs
@@ -0,0 +1,34 @@
+﻿using Engineer.Engine;
+using Engineer.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engineer.Project
+{
+    class GameHelpers
+    {
+        private static int Counter = 0;
+
+        public static DrawnSceneObject createSprite(string Image, Vertex position, Vertex size, string Name = null )
+        {
+            if (Name == null)
+            {
+                Name = "SomeObject" + (++Counter);
+            }
+            SpriteSet SomeSpriteSet = new SpriteSet(Name);
+            SomeSpriteSet.Sprite.Add(ResourceManager.Images[Image]);
+            Sprite SomeSprite = new Sprite();
+            SomeSprite.SpriteSets.Add(SomeSpriteSet);
+            SomeSprite.Scale = new Vertex(300, 40, 0);
+            SomeSprite.Translation = new Vertex(20, 20, 0);
+
+            DrawnSceneObject DrawnObject = new DrawnSceneObject();
+            DrawnObject.Visual = SomeSprite;
+            return DrawnObject;
+
+        }
+    }
+}
--- Engineer.Project/Player.cs
+++ Engineer.Project/Player.cs
@@ -11,13 +11,19 @@ namespace Engineer.Project
     public class Player:DrawnSceneObject
     {
         public static int id=1;
-        private int _Heat;                    
+        private int _Heat;
+        private int MaxHeat;
+
+        private Scene2D Scene;
+        private Sprite HealthBar;
 
         public Player(Scene2D CScene)
         {
-            _Heat = 100;
+            this.Scene = CScene;
+            MaxHeat = _Heat = 1000;
+
             //DrawnSceneObject Player = GameScene.CreateStaticTile("Player"+id++, ResourceManager.Images["kuglica_01"], new Vertex(100*id, 100 * id, 0), new Vertex(100 , 100 , 0), true);
-            SpriteSet Player = new SpriteSet("Player"+id);
+            SpriteSet Player = new SpriteSet("Player" + id);
             Player.Sprite.Add(ResourceManager.Images["kuglica_01"]);
 
             Sprite PlayerSprite = new Sprite();
@@ -25,7 +31,19 @@ namespace Engineer.Project
 
             this.Visual = PlayerSprite;
             this.Visual.Scale = new Vertex(50, 50, 0);
-            this.Visual.Translation = new Vertex(100*id, 100*id, 0);
+            this.Visual.Translation = new Vertex(100 * id, 100 * id, 0);
+            CScene.Events.Extern.TimerTick += new GameEventHandler(GameUpdate);
+
+
+            this.Data["Player"] = true;
+
+            DrawnSceneObject HealthBar = GameHelpers.createSprite("progressbar", new Vertex(20, 20, 0), new Vertex(300, 40, 0));
+
+            this.HealthBar = (Sprite) HealthBar.Visual;
+            this.HealthBar.Fixed = true;
+           
+            Scene.AddSceneObject(HealthBar);
+
         }
 
         public int Heat
@@ -33,5 +51,31 @@ namespace Engineer.Project
             get { return _Heat; }
             set { _Heat = value; }
         }
+
+        public void GameUpdate(Game G, EventArguments E)
+        {
+            if (Scene.GetObjectsWithData("HeatSource").Count > 0)
+            {
+
+            }
+            else if (Scene.GetObjectsWithData("ColdSource").Count > 0) {
+
+            } else {
+
+                List<SceneObject> Players = Scene.GetObjectsWithData("Player");
+
+                foreach(SceneObject Player in Players)
+                {
+                    if (Player != this)
+                    {
+                        int b = 1;
+                    }
+                }
+
+                this._Heat -= 1;
+            }
+            HealthBar.Scale = new Vertex(((float)_Heat / (float)MaxHeat) * 300.0f, HealthBar.Scale.Y, 0);
+        }
+
     }
 }
